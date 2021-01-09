3b0ac84954222461082cb1e756ef3ccc627eef09
alvinchandra99 <74279384+alvinchandra99@users.noreply.github.com>
2021-01-09 20:47:00 +0700

    Nambahin Requirement.txt

--- stock_price.py
+++ stock_price.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
-import base64
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
-import yfinance as yf
-import decimal
+
 
 st.title('Menakar Kekuatan Investor Retail di Pasar Modal Indonesia')
 
